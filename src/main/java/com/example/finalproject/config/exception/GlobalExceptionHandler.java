f2ff3e804267454f49a4d6163e6e26cbd1cf9461
MohammedErkinoff <erkinovmuhammadayubps@gmail.com>
2025-04-22 07:13:33 +0500

    fix exception handler

--- src/main/java/com/example/finalproject/config/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/finalproject/config/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.ResponseStatusException;
@@ -26,6 +27,20 @@ public class GlobalExceptionHandler {
         errorDto.setPath(request.getRequestURI());
 
         return new ResponseEntity<>(errorDto, ex.getStatusCode());
+    }
+
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    public ResponseEntity<ErrorDto> httpMessageNotReadableException(Exception ex, HttpServletRequest request) {
+
+        ErrorDto errorDto=new ErrorDto();
+        errorDto.setTimestamp(LocalDateTime.now());
+        errorDto.setStatus(400);
+        errorDto.setError("Invalid request");
+        errorDto.setPath(request.getRequestURI());
+
+        System.out.println("httpMessageNotReadableException: "+ex);
+
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler({SignatureException.class, MalformedJwtException.class})
@@ -59,7 +74,7 @@ public class GlobalExceptionHandler {
         errorDto.setTimestamp(LocalDateTime.now());
         errorDto.setStatus(500);
         errorDto.setError("Internal Error");
-        System.out.println(ex);
+        System.out.println("exception: "+ex);
         errorDto.setPath(request.getRequestURI());
 
         return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
